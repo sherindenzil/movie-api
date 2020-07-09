100147d319d5caf42aeee136bc819c0488886753
Sherin-Denzil <nysherin.denzil@gmail.com>
2020-07-09 11:20:35 -0400

    connected with remote database mongodb Atlas

--- index.js
+++ index.js
@@ -12,10 +12,19 @@ const Models = require("./models.js");
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect("mongodb://localhost:27017/myFlixDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+//mongoose.connect("mongodb://localhost:27017/myFlixDB", {
+// useNewUrlParser: true,
+//useUnifiedTopology: true,
+//});
+
+mongoose.connect(
+  process.env.CONNECTION_URI,
+
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const app = express();
 
