fd4eef9c1732d367e0197c8ff251035c62059df0
Sherin-Denzil <nysherin.denzil@gmail.com>
2020-07-26 00:13:48 -0400

    initial commit

---
+++ my-flix/client/src/actions/actions.js
@@ -0,0 +1,15 @@
+export const SET_MOVIES = "SET_MOVIES";
+export const SET_FILTER = "SET_FILTER";
+export const SET_LOGGEDIN = "SET_LOGGEDIN_USER";
+
+export function setMovies(value) {
+  return { type: SET_MOVIES, value };
+}
+
+export function setFilter(value) {
+  return { type: SET_FILTER, value };
+}
+
+export function setLoggedInUser(value) {
+  return { type: SET_LOGGEDIN, value };
+}
